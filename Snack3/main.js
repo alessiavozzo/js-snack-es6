bf9e2aec4593b0bf975a05fcbbbb9013d9d16fa3
alessiavozzo <a.vozzo95@gmail.com>
2024-03-05 15:10:24 +0100

    add snack3

--- Snack3/main.js
+++ Snack3/main.js
@@ -1,1 +1,40 @@
-console.log("ok");
+const myAnimals = [
+    {
+        nome: 'leone',
+        famiglia: 'felidi',
+        classe: 'mammiferi'
+    },
+    {
+        nome: 'cane',
+        famiglia: 'canidi',
+        classe: 'mammiferi'
+    },
+    {
+        nome: 'gallina',
+        famiglia: 'fasianidi',
+        classe: 'uccelli'
+    },
+    {
+        nome: 'gatto',
+        famiglia: 'felidi',
+        classe: 'mammiferi'
+    },
+    {
+        nome: 'echidna',
+        famiglia: 'tachiglossidi',
+        classe: 'mammiferi'
+    },
+    {
+        nome: 'piccione',
+        famiglia: 'colombidi',
+        classe: 'uccelli'
+    },
+    {
+        nome: 'pitone',
+        famiglia: 'pitonidi',
+        classe: 'rettili'
+    },
+]
+
+const mammaliaAnimals = myAnimals.filter(animal => animal.classe === "mammiferi");
+console.log(mammaliaAnimals);
